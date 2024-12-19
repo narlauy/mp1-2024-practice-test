52a1b6570e6099c2384f8525f3055d8c44dfa3e1
narlauy <guzeev2006@gmail.com>
2024-12-19 10:05:24 +0300

    Update task3.c

--- 3824Б1ПР5/04 Гузеев Захар Александрович/task3.c
+++ 3824Б1ПР5/04 Гузеев Захар Александрович/task3.c
@@ -1,6 +1,15 @@
 // Задача 3. Дана строка s.
 // Подсчитать количество содержащихся в ней строчных букв английского алфавита.
-size_t task3(char *s)
-{
-  return -1;
+#include <stdio.h>
+#include <ctype.h>
+
+size_t task3(char* s) {
+    size_t count = 0;
+    while (*s) {
+        if (islower(*s)) {
+            count++;
+        }
+        s++;
+    }
+    return count;
 }
